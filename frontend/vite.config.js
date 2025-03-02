e944a3211d21b9371f77ef50de57aa9d125ff747
NavinkumarJ <navinkumar02032005@gmail.com>
2025-03-02 18:48:13 +0530

    builded

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure Vite outputs files in "dist"
-  },
+  base: "./",  // Ensure correct relative path
 });
